5f41c3d7ef66428ce664afd6154f3cbd1dea80c1
maxshuang <maxshuang@tencent.com>
2019-08-04 21:04:39 +0800

    提交bubble sort代码

---
+++ sort/exchange/bubble_sort.cpp
@@ -0,0 +1,41 @@
+/*
+* 冒泡排序：交换排序，时间复杂度O(n^2), 空间复杂度O(1)。
+*
+*/
+#include "comm_tools.h"
+
+void bubble_sort(int* arr, int size)
+{
+  if(!arr || !size){
+    return;
+  }
+  
+  bool bSeq = true;
+  int tmp = 0;
+  int i = 0;
+  for(; i < size-1; ++i){
+     int j = size -1;
+     for(; j > i; --j){
+       if(arr[j] > arr[j-1]){
+         tmp = arr[j];
+         arr[j] = arr[j-1];
+         arr[j-1] = tmp;
+         bSeq = false;
+       }
+     }
+  
+    if(bSeq){
+      break;
+    }
+  }
+}
+
+#if 1
+int main()
+{
+  int arr[] = {10, 5, 8, 0, 2, 11, 6, 7, 6, 1};
+  int size = sizeof(arr)/sizeof(int);
+  bubble_sort(arr, size);
+  print_out(arr, size);
+}
+#endif
