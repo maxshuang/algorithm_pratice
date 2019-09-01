eb33cd416f77847c35e9bbe8b2f697f5363308cc
maxshuang <maxshuang@tencent.com>
2019-09-01 15:21:35 +0800

    commit convex hull

---
+++ stack_queue/convex_hull.cpp
@@ -0,0 +1,7 @@
+/*
+ *  application:(1)find farthest pair (2) robot avoid obstacle
+ *  intuition: (1) Can traverse the convex hull by making only counterclockwise turns.
+ *  (2) The vertices of convex hull appear in increasing order of polar angle
+*    with respect to point p with lowest y-coordinate
+ *
+ */
